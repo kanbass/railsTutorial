48fdb85f0939ba9007287e05ec36c445248659ca
kanbass <knb1031_ba@icloud.com>
2020-12-30 10:08:03 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+#カスタムヘルパー
+#Railsのビューでは膨大な数の組み込み関数が使えますが、それだけでなく、新しく作成することもできます。
 module ApplicationHelper
+    # ページごとの完全なタイトルを返します。
+    def full_title(page_title = '')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
