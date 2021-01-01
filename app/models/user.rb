a17171b67acad97f1669990b049a87e41504b221
kanbass <knb1031_ba@icloud.com>
2021-01-01 10:10:13 +0900

    userモデルの一意性追加(より現場で使われている風に)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    #before_save { self.email = email.downcase }#右式のselfは省略可能、左式は省略不可。そのため右式はself.email.downcaseと同じ
+    before_save { email.downcase! }#破壊的メソッドには「！」が付きます。これは呼び出し元の変数が変わってしまう可能性を含むメソッドのこと。
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :name,presence: true,length:{maximum: 50}
     validates :email,presence: true,length:{maximum: 255},format: {with: VALID_EMAIL_REGEX},uniqueness: { case_sensitive: false }
